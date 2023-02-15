2f50ca3ad545ac016948f7648fecabbbb92ce263
Nadya <nadya0707@yandex.ru>
2023-02-16 00:46:33 +0300

    commit 1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов, которые вводит пользователь, 
+// и выводит их на экран.
+
+Console.WriteLine("Введите восемь элементов массива: ");
+int [ ] array = Console.ReadLine().Split (" ").Select(x=>int.Parse(x)).ToArray(); 
+for (int i=0; i<array.Length; i++)
+array [i] = array [i];
+if (array.Length<=8) 
+Console.WriteLine($"[{string.Join(" , " , array)}]");
+else 
+Console.WriteLine($"Error!!! Количество элементов массива превышает допустимый размер");
